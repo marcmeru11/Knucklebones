8d6f262950eb5fba8fce0a1099dff66cc48497f5
MarcMeRu11 <marcmeru@gmail.com>
2026-04-22 11:12:11 +0200

    gitignore update 1/2

---
+++ src/js/services/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "FIREBASE_AUTHDOMAIN",
+  projectId: "FIREBASE_PROJECTID",
+  databaseURL: "FIREBASE_DATABASEURL",
+  storageBucket: "FIREBASE_STORAGEBUCKET",
+  messagingSenderId: "FIREBASE_MESSAGINGSENDERID",
+  appId: "FIREBASE_APPID",
+  measurementId: "FIREBASE_MEASUREMENTID"
+};
